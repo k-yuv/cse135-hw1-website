8816484a941018c93df4778532930163cb312b0b
Keyura Valalla <ke7nv21@gmail.com>
2026-01-29 19:34:34 -0800

    php

--- www/hw2/php/hello-html-php.php
+++ www/hw2/php/hello-html-php.php
@@ -1,5 +1,17 @@
-<?php
-echo "Hello, World!";
-$date = localtime();
-echo "<p>This program was generated at: $date</p>";
-?>
+<!DOCTYPE html>
+<html>;
+<head>
+<title>Hello CGI World</title>
+</head>
+<body>
+
+<h1 align=center>Hello HTML World</h1><hr/>
+<p>Hello World</p>
+<p>This page was generated with the PHP programming langauge</p>
+
+<p>This program was generated at: <?php $date =  date('Y-m-d H:i:s');
+                                        echo $date; 
+?></p>
+
+</body>
+</html>
