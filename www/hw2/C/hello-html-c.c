bab02d2401adb7dbaf34ec81fbf7027d1186ac80
Annejulia Milian <156250774+monbebe0319@users.noreply.github.com>
2026-01-31 20:13:27 -0800

    completed hello-html-c.c file

---
+++ www/hw2/C/hello-html-c.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+static const char *getEnv(const char *IPAddress, const char *unknownIP){
+    const char *value = getenv(IPAddress);
+    if (value == NULL) {
+        return unknownIP;
+    }
+    return value;
+}
+
+int main(void){
+    const char *visitorIP = getEnv("REMOTE_ADDR", "Unknown IP Address");
+    
+    time_t now = time(NULL);
+    char *timeString = ctime(&now);
+
+    if(timeString == NULL){
+        fprintf(stderr, "Unknown time.\n");
+    }
+
+    printf("Content-Type: text/html\r\n\r\n");
+    printf("<!DOCTYPE html>\n");
+    printf("<html lang=\"en\">\n");
+    printf("<head>\n");
+    printf("  <meta charset=\"UTF-8\" />\n");
+    printf("  <title>Hello HTML World</title>\n");
+    printf("</head>\n");
+    printf("<body>\n");
+
+    printf("  <h1>Hello HTML World</h1>\n");
+    printf("  <hr>\n");
+    printf("  Hello World<br>\n");
+    printf("  Language: C (CGI)<br>\n");
+    printf("  This program was generated at: %s<br>\n", timeString);
+    printf("  Your current IP address is: %s\n", visitorIP);
+
+    printf("</body>\n");
+    printf("</html>\n");
+
+    return 0;
+}
