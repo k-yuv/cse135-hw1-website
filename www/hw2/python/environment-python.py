3f486a9b638fb8b1ce6ec20164720490b96d3772
P4-Joshi-Dishita <djoshi894@student.fuhsd.org>
2026-01-31 20:13:38 -0800

    added env python

---
+++ www/hw2/python/environment-python.py
@@ -0,0 +1,23 @@
+import os
+
+print("Cache-Control: no-cache")
+print("Content-Type: text/html\n")
+
+print("""<!DOCTYPE html>
+<html>
+<head>
+    <title>Environment Variables</title>
+</head>
+<body>
+<h1 align="center">Environment Variables</h1>
+<hr>
+""")
+
+for variable in sorted(os.environ):
+    value = os.environ[variable]
+    print(f"<b>{variable}:</b> {value}<br />")
+
+print("""
+</body>
+</html>
+""")
