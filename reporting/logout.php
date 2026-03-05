db0316339eabeb640fc3f15f5b8702d2f393d117
Keyura Valalla <ke7nv21@gmail.com>
2026-03-05 14:14:45 -0800

    changed logout text

--- reporting/logout.php
+++ reporting/logout.php
@@ -4,6 +4,6 @@
    unset($_SESSION["password"]);
    unset($_SESSION["valid"]);
    
-   echo '<h4>You have cleaned session</h4>';
+   echo '<h4>You have been logged out. Redirecting to Login Page...</h4>';
    header('Refresh: 2; URL = login.php');
 ?>
