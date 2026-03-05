3b160f184ee932f4c4552695c2a60f7273bbe263
Keyura Valalla <ke7nv21@gmail.com>
2026-03-05 14:10:54 -0800

    unset all session variables

--- reporting/logout.php
+++ reporting/logout.php
@@ -2,6 +2,7 @@
    session_start();
    unset($_SESSION["username"]);
    unset($_SESSION["password"]);
+   unset($_SESSION["valid"]);
    
    echo '<h4>You have cleaned session</h4>';
    header('Refresh: 2; URL = login.php');
