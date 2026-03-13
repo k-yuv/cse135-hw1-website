94d9add6ad4e2ffc29f25cc0f0b9b718203fc848
Keyura Valalla <ke7nv21@gmail.com>
2026-03-13 12:09:38 -0700

    cvhjk

--- reporting/dashboard.php
+++ reporting/dashboard.php
@@ -161,7 +161,7 @@
     </div>
     <div class="right-navbar d-flex align-items-center">
         <a href="logout.php">Logout</a>
-        <p class='me-3'> <?php echo $_SESSION['username'] ?> </p>
+        <p class='me-3 d-block text-center'> <?php echo $_SESSION['username'] ?> </p>
     </div>
 </nav>
 
