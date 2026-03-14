56b231392131d765e7324d124a31cdcbccdcc860
Keyura Valalla <ke7nv21@gmail.com>
2026-03-13 17:39:51 -0700

    editted errors page title

--- reporting/errors.php
+++ reporting/errors.php
@@ -103,7 +103,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Errors ♡</title>
+    <title>Errors !!</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.7/dist/chart.umd.min.js"></script>
